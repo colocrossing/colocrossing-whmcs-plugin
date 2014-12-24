2e81fbb81741bb911f4caf1a0870eb64fa53c27c
Matt Andre <mandre@colocrossing.com>
2014-12-24 16:54:12 -0500

    Implement rDNS and Null Routes for Subnets on CLient Side

--- modules/addons/colocrossing/clients/controllers/NullRoutesController.php
+++ modules/addons/colocrossing/clients/controllers/NullRoutesController.php
@@ -11,11 +11,62 @@ if(!defined('WHMCS')) {
 class ColoCrossing_Clients_NullRoutesController extends ColoCrossing_Clients_Controller {
 
 	public function create(array $params) {
+		$this->subnet = $this->api->subnets->find($params['id']);
+		$this->device = isset($this->subnet) ? $this->subnet->getDevice() : null;
 
+		if(empty($this->subnet) || empty($this->device) || empty($this->current_user)
+			|| !$this->current_user->hasPermissionForDevice($this->device)) {
+			$this->setFlashMessage('The subnet was not found.', 'error');
+			$this->redirectTo('error', 'missing');
+		}
+
+		$expire_date = max(min(strtotime('+' . $params['duration']), strtotime('+30 days')), time());
+
+		$this->null_route = $this->subnet->addNullRoute($params['ip_address'], $params['comment'], $expire_date);
+
+		if($this->null_route === false) {
+			$this->setFlashMessage('An error occurred while adding null route.', 'error');
+		} else {
+			ColoCrossing_Model_Event::log('Null route for ' . $params['ip_address'] . ' was added.');
+			$this->setFlashMessage('The null route for ' . $params['ip_address'] . ' was added successfully!', 'success');
+		}
+
+		$this->redirectTo('subnets', 'view', array(
+			'id' => $params['subnet_id']
+		));
 	}
 
 	public function destroy(array $params) {
+		$this->null_route = $this->api->null_routes->find($params['id']);
 
+		if(empty($this->null_route)) {
+			$this->setFlashMessage('The null route was not found.', 'error');
+			$this->redirectTo('error', 'missing');
+		}
+
+		$this->subnet = $this->null_route->getSubnet();
+		$this->device = isset($this->subnet) ? $this->subnet->getDevice() : null;
+
+		if(empty($this->subnet) || empty($this->device) || empty($this->current_user)
+			|| !$this->current_user->hasPermissionForDevice($this->device)) {
+			$this->setFlashMessage('The subnet was not found.', 'error');
+			$this->redirectTo('error', 'missing');
+		}
+
+		$this->ip_address = $this->null_route->getIpAddress();
+
+		if(!$this->null_route->isRemovable()) {
+			$this->setFlashMessage('The null route on ' . $this->ip_address . ' is not removable.', 'error');
+		} else if($this->null_route->remove()) {
+			ColoCrossing_Model_Event::log('Null route on ' . $this->ip_address . ' was removed.');
+			$this->setFlashMessage('The null route on ' . $this->ip_address . ' was removed successfully!', 'success');
+		} else {
+			$this->setFlashMessage('An error occurred while removing null route on ' . $this->ip_address . '.', 'error');
+		}
+
+		$this->redirectTo('subnets', 'view', array(
+			'id' => $this->subnet->getId()
+		));
 	}
 
 }
--- modules/addons/colocrossing/clients/controllers/SubnetsController.php
+++ modules/addons/colocrossing/clients/controllers/SubnetsController.php
@@ -20,7 +20,45 @@ class ColoCrossing_Clients_SubnetsController extends ColoCrossing_Clients_Contro
 			$this->redirectTo('error', 'missing');
 		}
 
+		$this->ip_addresses = $this->subnet->getIpAddresses();
+
+		$this->rdns_records = $this->subnet->isReverseDnsEnabled() ? $this->subnet->getReverseDNSRecords() : null;
+		$this->null_routes = $this->subnet->isNullRoutesEnabled() ? ColoCrossing_Utility::getMapCollection($this->subnet->getNullRoutes(), 'ip_address') : null;
+
 		return $this->subnet->getCIDRIpAddress();
+	}
+
+	public function update(array $params) {
+		$this->subnet = $this->api->subnets->find($params['id']);
+		$this->device = isset($this->subnet) ? $this->subnet->getDevice() : null;
+
+		if(empty($this->subnet) || empty($this->device) || empty($this->current_user)
+			|| !$this->current_user->hasPermissionForDevice($this->device)) {
+			$this->setFlashMessage('The subnet was not found.', 'error');
+			$this->redirectTo('error', 'missing');
+		}
+
+		if(!$this->subnet->isReverseDnsEnabled()) {
+			$this->setFlashMessage('Reverse DNS records are not enabled for this subnet.', 'error');
+		} else if($this->subnet->isPendingServiceRequest()) {
+			$this->setFlashMessage('Reverse DNS records can\'t be edited while a service request is pending.', 'error');
+		} else {
+			$request_result = $this->subnet->updateReverseDNSRecords($params['rdns_records']);
+
+			if($request_result === true) {
+				ColoCrossing_Model_Event::log('Reverse DNS records for ' . $this->subnet->getCIDRIpAddress() . ' were updated.');
+				$this->setFlashMessage('Reverse DNS records have successfully been edited.', 'success');
+			} else if($request_result === false) {
+				$this->setFlashMessage('An error occurred while saving reverse DNS records.', 'error');
+			} else {
+				ColoCrossing_Model_Event::log('Reverse DNS record service request for ' . $this->subnet->getCIDRIpAddress() . ' was submitted.');
+				$this->setFlashMessage('Reverse DNS record service request has successfully been submitted.', 'success');
+			}
+		}
+
+		$this->redirectTo('subnets', 'view', array(
+			'id' => $this->subnet->getId()
+		));
 	}
 
 }
