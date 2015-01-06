57aae1691de48d3d57e47c08078a8a801e4fc27e
Matt Andre <mandre@colocrossing.com>
2015-01-06 13:32:24 -0500

    Fix The API URL

--- modules/addons/colocrossing/api/src/ColoCrossing/Client.php
+++ modules/addons/colocrossing/api/src/ColoCrossing/Client.php
@@ -18,7 +18,7 @@ class ColoCrossing_Client
 	 */
 	private static $DEFAULT_OPTIONS = array(
 		'application_name' => 'ColoCrossing PHP API Client',
-		'api_url' => 'https://portal.colocrossing/api/',
+		'api_url' => 'https://portal.colocrossing.com/api/',
 		'api_version' => 1,
 		'request_timeout' => 60,
 		'connection_timeout' => 5,
