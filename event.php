f5eabbcc3e6184019edbff67892b22ad1ae44e79
Matt Andre <mandre@colocrossing.com>
2015-11-04 16:47:20 -0500

    Fix Issue with Abuse Ticket Propagation on NGINX

--- event.php
+++ event.php
@@ -1,5 +1,20 @@
 <?php
 
+//Add getallheaders() if Running on NGINX
+if(!function_exists('getallheaders')) {
+    function getallheaders() {
+        $headers = array();
+
+        foreach ($_SERVER as $name => $value) {
+            if(substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+
+        return $headers;
+    }
+}
+
 //Get Request Headers
 $headers = getallheaders();
 
