9ab7f47573f6c97743ec889b15914821068ae24e
Dylan Telford <dylantelford@gmail.com>
2018-05-07 15:50:55 -0400

    ipmi null routes

--- modules/addons/colocrossing/api/src/ColoCrossing/Object/Device/IpmiConfiguration.php
+++ modules/addons/colocrossing/api/src/ColoCrossing/Object/Device/IpmiConfiguration.php
@@ -4,8 +4,7 @@
  * Represents the Ipmi Configuration of a Device. Provides methods for 
  * retrieving the hostname and, if one exists, information about the 
  * null route, specifically its status and the time it will be replaced
- * if it is lifted. Also prvides methods for lifting, renewing a lift, 
- * and replacing a null route. 
+ * if it is lifted. 
  *
  * @category   ColoCrossing
  * @package    ColoCrossing_Object
@@ -13,7 +12,11 @@
  */
 class ColoCrossing_Object_Device_IpmiConfiguration extends ColoCrossing_Object
 {
-
+	/**
+	 * Retrieve the hostname ipmi is configured to.
+	 * 
+	 * @return string	The Hostname
+	 */
 	public function getHostname()
 	{
 		$hostname = $this->getValue('hostname');
@@ -21,6 +24,12 @@ class ColoCrossing_Object_Device_IpmiConfiguration extends ColoCrossing_Object
 		return empty($hostname) ? null : $hostname;
 	}
 
+	/**
+	 * Retrieve an array with information about the null route, including its status
+	 * and the time it will be replaced if it is currently lifted.
+	 * 
+	 * @return array	An associative array describing the null route.
+	 */
 	public function getNullRoute()
 	{
 		$null_route = $this->getValue('null_route');
@@ -28,6 +37,16 @@ class ColoCrossing_Object_Device_IpmiConfiguration extends ColoCrossing_Object
 		return empty($null_route) ? null : $null_route;
 	}
 
+	/**
+	 * Retrieve an array with information about the null route's status.
+	 * Contains an 'id' and a 'name' as follows:
+	 *  1 => 'active',
+	 *  2 => 'lifted',
+	 *  3 => 'awaiting routing', (temporarily removed and awaiting a routing change.)
+	 *  4 => 'archived'
+	 * 
+	 * @return array	An associative array describing the null route.
+	 */
 	public function getNullRouteStatus()
 	{
 		$null_route = $this->getNullRoute();
@@ -35,6 +54,11 @@ class ColoCrossing_Object_Device_IpmiConfiguration extends ColoCrossing_Object
 		return isset($null_route) ? $null_route['status'] : null;
 	}
 
+	/**
+	 * Retrieve the time the null route will be replaced if it is currently lifted.
+	 * 
+	 * @return int	A timestamp for when the null will be replaced.
+	 */
 	public function getNullRouteLiftExpiration()
 	{
 		$status = $this->getNullRouteStatus();
@@ -50,5 +74,5 @@ class ColoCrossing_Object_Device_IpmiConfiguration extends ColoCrossing_Object
 
 		return null;
 	}
-	
+
 }
